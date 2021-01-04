d0eb1947d72559e3cc977039d8757cb3f51786a5
Gustavo Riquena <gustavo_riquena1@outlook.com>
2021-01-04 16:54:23 -0300

    First Commit SpringBoot and Rest

---
+++ 00 RestSpringBootUdemy Spring Initializer/src/main/java/com/javaudemy/restspringbootudemy/RestSpringBootUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.javaudemy.restspringbootudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringBootUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringBootUdemyApplication.class, args);
+	}
+
+}
---
+++ 00 RestSpringBootUdemy Spring Initializer/src/test/java/com/javaudemy/restspringbootudemy/RestSpringBootUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaudemy.restspringbootudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
