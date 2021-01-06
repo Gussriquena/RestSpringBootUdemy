b67fefceedf56fa62d729830b9c29b2f5b2e4ac7
Gustavo Riquena <gustavo_riquena1@outlook.com>
2021-01-06 01:24:49 -0300

    implementing POST, PUT, DELETE

--- 02 RestSpringBootUdemy Scaffold Calculator/src/main/java/com/javaudemy/services/PersonServices.java
+++ 02 RestSpringBootUdemy Scaffold Calculator/src/main/java/com/javaudemy/services/PersonServices.java
@@ -1,5 +1,7 @@
 package com.javaudemy.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Service;
@@ -11,6 +13,21 @@ public class PersonServices {
 
 	private final AtomicLong counter = new AtomicLong();
 	
+	public Person create(Person person) {
+		
+		return person;
+	}
+	
+	public Person update(Person person) {
+		
+		return person;
+	}
+	
+	public void delete(String id) {
+		
+	}
+	
+	
 	public Person findById(String id) {
 		Person person = new Person();
 		
@@ -20,6 +37,27 @@ public class PersonServices {
 		person.setAddress("Jandira, SP, Brazil");
 		person.setGenger("Male");
 		
+		return person;
+	}
+	
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<Person>();
+		
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		
+		return persons;
+	}
+
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person Name " + i);
+		person.setLastName("Last Name " + i);
+		person.setAddress("Some Address in Brazil " + i);
+		person.setGenger("Male");
 		return person;
 	}
 	
