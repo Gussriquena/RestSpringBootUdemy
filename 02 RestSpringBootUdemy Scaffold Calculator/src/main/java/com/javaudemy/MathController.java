e497dbdd527f70f749e924287b69bcf94620b093
Gustavo Riquena <gustavo_riquena1@outlook.com>
2021-01-05 23:21:10 -0300

    Code refactoring

--- 02 RestSpringBootUdemy Scaffold Calculator/src/main/java/com/javaudemy/MathController.java
+++ 02 RestSpringBootUdemy Scaffold Calculator/src/main/java/com/javaudemy/MathController.java
@@ -5,84 +5,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.javaudemy.exception.UnsupportedMathOperationException;
+import com.javaudemy.utils.MathUtils;
 
 @RestController
 public class MathController {
 	
-	private final String NOT_NUMERIC_VALUE = "Please, set a numeric value!";
+	MathUtils mathUtils = new MathUtils();
 	
 	@RequestMapping(value="/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
 	public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-		
-		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-			throw new UnsupportedMathOperationException(NOT_NUMERIC_VALUE);
-		}
-		
-		return convertToDouble(numberOne) + convertToDouble(numberTwo);
+		mathUtils.validateUnsupportableNumbers(numberOne, numberTwo);
+		return mathUtils.convertToDouble(numberOne) + mathUtils.convertToDouble(numberTwo);
 	}
 	
 	@RequestMapping(value="/subtraction/{numberOne}/{numberTwo}", method = RequestMethod.GET)
 	public Double subtraction(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception{
-		
-		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-			throw new UnsupportedMathOperationException(NOT_NUMERIC_VALUE);
-		}
-		 
-		return convertToDouble(numberOne) - convertToDouble(numberTwo);
+		mathUtils.validateUnsupportableNumbers(numberOne, numberTwo);
+		return mathUtils.convertToDouble(numberOne) - mathUtils.convertToDouble(numberTwo);
 	}
 	
 	@RequestMapping(value="/multiplication/{numberOne}/{numberTwo}", method = RequestMethod.GET)
 	public Double multiplication(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-			throw new UnsupportedMathOperationException(NOT_NUMERIC_VALUE);
-		}
-		
-		return convertToDouble(numberOne) * convertToDouble(numberTwo);
+		mathUtils.validateUnsupportableNumbers(numberOne, numberTwo);
+		return mathUtils.convertToDouble(numberOne) * mathUtils.convertToDouble(numberTwo);
 	}
 	
 	@RequestMapping(value="/division/{numberOne}/{numberTwo}", method = RequestMethod.GET)
 	public Double division(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-			throw new UnsupportedMathOperationException(NOT_NUMERIC_VALUE);
-		}
-		
-		return convertToDouble(numberOne) / convertToDouble(numberTwo);
+		mathUtils.validateUnsupportableNumbers(numberOne, numberTwo);
+		return mathUtils.convertToDouble(numberOne) / mathUtils.convertToDouble(numberTwo);
 	}
-	
 	
 	@RequestMapping(value="/average/{numberOne}/{numberTwo}", method = RequestMethod.GET)
 	public Double average(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-			throw new UnsupportedMathOperationException(NOT_NUMERIC_VALUE);
-		}
-		
-		return (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2;
+		mathUtils.validateUnsupportableNumbers(numberOne, numberTwo);
+		return (mathUtils.convertToDouble(numberOne) + mathUtils.convertToDouble(numberTwo)) / 2;
 	}
 	
 	@RequestMapping(value="/square/{number}", method = RequestMethod.GET)
 	public Double square(@PathVariable("number") String number) throws Exception {
-		if (!isNumeric(number)) {
-			throw new UnsupportedMathOperationException(NOT_NUMERIC_VALUE);
-		}
-		
-		return Math.sqrt(convertToDouble(number));
-	}
-	
-	// MÉTODOS ÚTEIS
-	
-	private Double convertToDouble(String strNumber) {
-		if (strNumber == null) return 0D;
-		String number = strNumber.replaceAll(",", ".");
-		if(isNumeric(number)) return Double.parseDouble(number);
-		return 0D;
-	}
-
-	private boolean isNumeric(String strNumber) {
-		if (strNumber == null) return false;
-		
-		String number = strNumber.replaceAll(",", ".");
-		return number.matches("[+-]?[0-9]*\\.?[0-9]+");
+		mathUtils.validateUnsupportableNumbers(number, "");
+		return Math.sqrt(mathUtils.convertToDouble(number));
 	}
 	
 }
